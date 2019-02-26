afc67f444133f70e08d9417a182bfcb80696a219
Fogwoggler <Fogwoggler@Fogwoggler>
2019-02-25 20:14:14 -0700

    Second Commit

--- CSE360_Assignment2/src/cse360assign2/Calculator.java
+++ CSE360_Assignment2/src/cse360assign2/Calculator.java
@@ -15,8 +15,7 @@ public class Calculator {
 	
 	/**
 	 * This method creates a calculator class.
-	 */
-	
+	 */	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
@@ -24,8 +23,7 @@ public class Calculator {
 	/**
 	 * This class retrieves the total of the calculated values.
 	 * @return
-	 */
-	
+	 */	
 	public int getTotal () {
 		return 0;
 	}
@@ -33,44 +31,44 @@ public class Calculator {
 	/**
 	 * This is the add function of the calculator.
 	 * @param value
-	 */
-	
+	 */	
 	public void add (int value) {
-		
+		total = total + value;
 	}
 	
 	/**
 	 * This is the subtract function of the calculator.
 	 * @param value
-	 */
-	
+	 */	
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
 	
 	/**
 	 * This is the multiply function of the calculator.
 	 * @param value
-	 */
-	
+	 */	
 	public void multiply (int value) {
-		
+		total = total * value;
 	}
 	
 	/**
 	 * This is the Divide function of the calculator.
 	 * @param value
-	 */
-	
+	 */	
 	public void divide (int value) {
-		
+		if(value == 0) {
+			total = 0;
+		}
+		else {
+			total = total / value;
+		}
 	}
 	
 	/**
 	 * This prints out the history of the input.
 	 * @return
-	 */
-	
+	 */	
 	public String getHistory () {
 		return "";
 	}
